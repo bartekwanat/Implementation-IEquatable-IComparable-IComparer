ac88704a66e05a3c5edb86c262ad6cb2511f0dd9
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-04-11 13:49:20 +0200

    create Pracownik class with no whitespace in Nazwisko

---
+++ Implementation-IEquatable-IComparable-IComparer/Pracownik.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation_IEquatable_IComparable_IComparer
+{
+    public class Pracownik
+    {
+        private string nazwisko;
+        public string Nazwisko { 
+            get => nazwisko;
+            set => nazwisko = String.Concat(value.Where(c => !char.IsWhiteSpace(c)));
+                 }
+          
+        public DateTime? DataZatrudnienia { get; set; }
+        public decimal Wynagrodzenie { get; set; }
+
+    }
+}
--- Implementation-IEquatable-IComparable-IComparer/Program.cs
+++ Implementation-IEquatable-IComparable-IComparer/Program.cs
@@ -7,6 +7,12 @@ namespace Implementation_IEquatable_IComparable_IComparer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var pracownik = new Pracownik();
+            pracownik.Nazwisko = "    Bart     ";
+            pracownik.Wynagrodzenie = 100;
+            pracownik.DataZatrudnienia = new DateTime(2010, 10, 01);
+            Console.WriteLine(pracownik.Nazwisko);
+
         }
     }
 }
