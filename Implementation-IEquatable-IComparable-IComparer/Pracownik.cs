ac25cdbb5e429e39f1cbfefa83dce4560e140d2e
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-04-11 14:00:21 +0200

    add DataZatrudnienia validation

--- Implementation-IEquatable-IComparable-IComparer/Pracownik.cs
+++ Implementation-IEquatable-IComparable-IComparer/Pracownik.cs
@@ -13,8 +13,15 @@ namespace Implementation_IEquatable_IComparable_IComparer
             get => nazwisko;
             set => nazwisko = String.Concat(value.Where(c => !char.IsWhiteSpace(c)));
                  }
-          
-        public DateTime? DataZatrudnienia { get; set; }
+        private DateTime dataZatrudnienia;
+        public DateTime? DataZatrudnienia 
+        { 
+            get => dataZatrudnienia;
+            set {
+                if (dataZatrudnienia > DateTime.Now)
+                    throw new ArgumentException();
+            } 
+        }
         public decimal Wynagrodzenie { get; set; }
 
     }
